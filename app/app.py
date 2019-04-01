9de7f2086a7631e48ea207ba28a33212626a563b
Robert Silva <contato@robertsilva.com.br>
2019-03-31 22:37:42 -0300

    Model organization removed, User model add

--- app/app.py
+++ app/app.py
@@ -4,15 +4,17 @@ from db import db
 from config import Config
 from resources.check import Check
 from resources.organization import Organization, OrganizationList
+from resources.user import User, UserList
 
 app = Flask(__name__)
 app.config.from_object(Config)
 api = Api(app)
 
 api.add_resource(Check, '/check')
-api.add_resource(Organization, '/org/<string:name>')
+api.add_resource(User, '/user/<string:name>')
+api.add_resource(UserList, '/users')
+api.add_resource(Organization, '/org/<int:org_id>')
 api.add_resource(OrganizationList, '/orgs')
-
 
 if __name__ == '__main__':
     from db import db
--- app/models/organization.py
+++ app/models/organization.py
@@ -5,18 +5,27 @@ class OrganizationModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(80))
+    email = db.Column(db.String(80))
     status = db.Column(db.Integer, default=1)
 
-    def __init__(self, name, status=1):
+    def __init__(self,name, email, status=1):
         self.name = name
+        self.email = email
         self.status = status
 
     def json(self):
-        return {'name': self.name}
+        return {'id': self.id,
+        'name': self.name, 
+        'email': self.email,
+        'status': self.status}
 
     @classmethod
     def find_by_name(cls, name):
         return cls.query.filter_by(name=name).first()
+
+    @classmethod
+    def find_by_id(cls, org_id):
+        return cls.query.get_or_404(org_id)
 
     def save_to_db(self):
         db.session.add(self)
